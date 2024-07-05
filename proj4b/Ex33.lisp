d842a39d7c01d225eccb224c756aa2806d58618a
Luiz Augusto Bello <labma02@gmail.com>
2024-07-05 12:57:01 -0300

    feat: Add the files Ex32.lisp, Ex33.lisp, Ex34.lisp, Ex36.lisp, Ex37.lisp, and Ex38.lisp.

---
+++ proj4b/Ex33.lisp
@@ -0,0 +1,7 @@
+(defun raiz (a n)
+    (cond
+        ((< n 0) nil)
+        ((zerop n) 1)
+        (T (/ (+ (raiz a (- n 1)) (/ 3 (raiz a (- n 1)))) 2))
+    )
+)
---
+++ proj4b/Ex34.lisp
@@ -0,0 +1,8 @@
+(defun seriePI (n)
+    (cond
+        ((< n 1) nil)
+        ((eql n 1) 4)
+        ((eql (mod n 2) 0) (- (seriePI (- n 1))(/ 4 (- (* n 2) 1))))
+        (T (+ (seriePI (- n 1))(/ 4 (- (* n 2) 1))))
+    )
+)
